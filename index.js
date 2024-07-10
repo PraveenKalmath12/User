57c73bbdc68b8a0cee119ad307444456982b7a33
PraveenKalmath12 <praveenkalmathmypridegames@gmail.com>
2024-07-10 18:39:09 +0530

    second

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import express from 'express'; // Import express
 import cors from 'cors'; // Import cors
 import connectToMongoDB from './Adapters/Mongodb.adapter.js'; 
 import adminRoutes from './routes/Admin.routes.js'; 
-import userRoutes from './routes/User.routes.js'
+import userRoutes from './routes/User.routes.js';
+import { https } from 'firebase-functions';
+
 const app = express();
 
 app.use(cors());
@@ -14,7 +16,6 @@ connectToMongoDB();
 // Import and use admin routes
 app.use('/admin', adminRoutes);
 app.use('/api/v-1/user', userRoutes);
-const port = 5051;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
-});
+
+// Use Firebase Functions to export your Express app
+export const api = https.onRequest(app);
